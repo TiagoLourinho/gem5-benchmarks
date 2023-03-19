ec5e771f828a6c85cbb1628ef80ad01d85bfc599
Tiago Lourinho <tiagolourinho001@gmail.com>
2023-03-19 17:48:50 +0000

    Baseline: IPC = 4.771839

--- benchmarks/baseline.c
+++ benchmarks/baseline.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
 
 int main()
 {
-    int i1, i2, i3, i4, i5, i6; // 6 of FUList0 (IntAlu)
+    int i1, i2, i3, i4;
+    float f1, f2;
 
-    float f1, f2, f3, f4; // 4 of FUList2 (FloatAdd)
-
-    for (int i = 0; i < 1000000; i++)
+    for (int j = 0; j < 1000000; j++)
     {
         i1 = 1;
-        f1 = 1;
-
         i2 = 1;
-        f2 = 1;
-
         i3 = 1;
-        f3 = 1;
-
         i4 = 1;
-        f4 = 1;
 
-        i5 = 1;
-        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
     }
+
     return 0;
 }

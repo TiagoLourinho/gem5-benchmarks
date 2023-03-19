4a3de72617fe53e7cdb61015337e9e6c8c6c36c8
Tiago Lourinho <tiagolourinho001@gmail.com>
2023-03-19 13:29:00 +0000

    improved baseline

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gem5-benchmarks
 Designed benchmarks for the course of High Performance Computing Architectures
+
+# Useful commands
+
+`aarch64-linux-gnu-gcc -static benchmarks/baseline.c`
+
+`../gem5/build/ARM/gem5.opt config_files/two_level_ooo.py a.out`
--- benchmarks/baseline.c
+++ benchmarks/baseline.c
@@ -2,28 +2,26 @@
 
 int main()
 {
-    int q, w, e, r, t, y, u;
+    int i1, i2, i3, i4, i5, i6; // 6 of FUList0 (IntAlu)
+
+    float f1, f2, f3, f4; // 4 of FUList2 (FloatAdd)
 
     for (int i = 0; i < 1000000; i++)
     {
-        q = 0;
-        w = 0;
-        e = 0;
-        r = 0;
-        t = 0;
-        y = 0;
-        u = 0;
+        i1 = 1;
+        f1 = 1;
+
+        i2 = 1;
+        f2 = 1;
+
+        i3 = 1;
+        f3 = 1;
+
+        i4 = 1;
+        f4 = 1;
+
+        i5 = 1;
+        i6 = 1;
     }
     return 0;
 }
-
-/* #include <stdio.h>
-
-int main()
-{
-    for (int i = 0; i < 100000; i++)
-    {
-        printf("%d\n", i);
-    }
-    return 0;
-} */

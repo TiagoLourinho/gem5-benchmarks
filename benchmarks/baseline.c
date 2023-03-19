7e22e95aa125ddfcdf2d82d6b7cdedeffa75cfd3
Tiago Lourinho <tiagolourinho001@gmail.com>
2023-03-19 20:30:17 +0000

    Baseline: IPC = 5.009895

--- benchmarks/baseline.c
+++ benchmarks/baseline.c
@@ -1,16 +1,82 @@
 
 int main()
 {
-    int i1, i2, i3, i4;
-    float f1, f2;
+    int i1, i2, i3, i4, i5, i6; // FUList0 - 6 (IntAlu)
+    float f1, f2;               // FUList3 - 6 (FloatMisc)
 
+    // Repeat the same "basic block" of code sometimes
+    // to reduce the instructions wrongly fetched in the branch (8 contiguous instructions)
     for (int j = 0; j < 1000000; j++)
     {
         i1 = 1;
         i2 = 1;
         i3 = 1;
         i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
 
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
+        f1 = 1.1;
+        f2 = 1.1;
+
+        i1 = 1;
+        i2 = 1;
+        i3 = 1;
+        i4 = 1;
+        i5 = 1;
+        i6 = 1;
         f1 = 1.1;
         f2 = 1.1;
     }

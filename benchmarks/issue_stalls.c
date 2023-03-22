698ea0fea590f787abe3e18ca0aa8ecbef465bf6
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-03-22 19:38:11 +0000

    Update issue_stalls.c

--- benchmarks/issue_stalls.c
+++ benchmarks/issue_stalls.c
@@ -1,13 +1,131 @@
+#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int x = 1, y = 20, z = 10, k = 3;
-    for (int i = 0; i < 1000000; i++)
-    {
-        y = x + z;
-        x = z + z;
-        x = k + 2;
+    for (int i = 0; i< 30000; i++){
+	    
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+         y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+         y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+         y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+         y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+
+        y = sqrt(x / z);
+        x = sqrt(z/y);
+        x = k / sqrt(z);
+
+        x = k / sqrt(z);
+        x = sqrt(z/y);
+        y = sqrt(x / z);
+       
     }
 
     return 0;

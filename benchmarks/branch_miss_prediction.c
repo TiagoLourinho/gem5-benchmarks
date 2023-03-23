9b6077d9618f21e3c844b1bae34ada380b5599bd
Tiago Lourinho <tiagolourinho001@gmail.com>
2023-03-23 02:25:36 +0000

    added benchmark for branch

--- benchmarks/branch_miss_prediction.c
+++ benchmarks/branch_miss_prediction.c
@@ -1,8 +1,139 @@
-#include <stdio.h>
-
 int main()
 {
 
-    printf("Hello, World!");
-    return 0;
+    int count = 0, k, rand = 14, bit, i_copy;
+
+    for (int i = 0; i < 100000; i++)
+    {
+
+        i_copy = i;
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+
+        ////////// Basic Block //////////
+        rand ^= rand << 13;
+        rand ^= rand >> 17; // Generate random number
+        rand ^= rand << 5;
+
+        k = i_copy % 32;       // Select the bit to choose (int have 32 bits)
+        bit = (rand >> k) & 1; // Extract the bit in position k
+
+        i_copy++; // Change for the following basic block
+
+        if (bit)
+        {
+            count++;
+        }
+        /////////////////////////////////
+    }
 }

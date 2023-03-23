c73fd33502651abec75334ba443beed97ee2193c
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2023-03-23 00:22:07 +0000

    Update cache_misses_l1_hits_l2_miss.c

--- benchmarks/cache_misses_l1_hits_l2_miss.c
+++ benchmarks/cache_misses_l1_hits_l2_miss.c
@@ -1,8 +1,60 @@
 int main() {
-    int array[100000], x = 0, jump = 16;
+    int array[100000], x = 0, jump = 16, k = 0, v = 0, w = 0;
     for (int j = 0; j < jump; j++) {
         for (int i = j; i < 100000; i += jump) {
             array[i] = i;
+
+            k = k + 1;
+            k = w - 1;
+            k = k * w;
+            w = v + 1;
+            v = w + k;
+            v++;
+            w--;
+            k = 1;
+            v = 1;
+            w = 1;
+            k = k * v * w;
+            w = (v - 2) % 2;
+
+            k = k + 1;
+            k = w - 1;
+            k = k * w;
+            w = v + 1;
+            v = w + k;
+            v++;
+            w--;
+            k = 1;
+            v = 1;
+            w = 1;
+            k = k * v * w;
+            w = (v - 2) % 2;
+
+            k = k + 1;
+            k = w - 1;
+            k = k * w;
+            w = v + 1;
+            v = w + k;
+            v++;
+            w--;
+            k = 1;
+            v = 1;
+            w = 1;
+            k = k * v * w;
+            w = (v - 2) % 2;
+
+            k = k + 1;
+            k = w - 1;
+            k = k * w;
+            w = v + 1;
+            v = w + k;
+            v++;
+            w--;
+            k = 1;
+            v = 1;
+            w = 1;
+            k = k * v * w;
+            w = (v - 2) % 2;
         }
     }
 
